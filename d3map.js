92cc0ffc46c4e98725201c83a99417267ac0e0e0
Rael Watt <55397739+rnwatt@users.noreply.github.com>
2023-11-09 15:50:17 +0000

    Tidying up file

--- d3map.js
+++ d3map.js
@@ -1,49 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
   var svg;
-  var filteredData = [];
+  // Slider value variable
+  var sliderData = [];
   // Tooltip for displaying details on hover
   var tooltip = d3.select("body").append("div")
   .attr("class", "tooltip")
   .style("opacity", 0);
   function handleMouseOver(event, d) {
-    // Show details when hovering over a point
-    console.log("Hovered Data:", d);
-    if (!filteredData.length) return;
-  
+  //If there is no data on mouseover then ignore
+    if (!sliderData.length) return;
     tooltip.transition()
       .duration(200)
       .style("opacity", .9);
   
-      const hoveredData = d3.select(this).data()[0];
+      const hoverData = d3.select(this).data()[0];
     tooltip.html(`
-    <strong>Town:</strong> ${hoveredData.Town}<br>
-    <strong>Population:</strong> ${hoveredData.Population}<br>
-    <strong>County:</strong> ${hoveredData.County}`)
+    <strong>Town:</strong> ${hoverData.Town}<br>
+    <strong>Population:</strong> ${hoverData.Population}<br>
+    <strong>County:</strong> ${hoverData.County}`)
       .style("left", (event.pageX + 10) + "px")
       .style("top", (event.pageY - 28) + "px");
 }
-
+//Tooltip disappears
 function handleMouseOut(event, d) {
   tooltip.transition()
   .duration(500)
   .style("opacity", 0);
 }
 function LoadData(numPlots) {
-// Define the URL of the server feed
+  
     const jsonFeed = 'http://34.38.72.236/Circles/Towns/50';
-
-    
-
 
 // Fetch data from the server
 d3.json(jsonFeed, function(error, data) {
       if (error) {
         console.error('Error fetching data:', error);
       } else {
-        console.log('Fetched data:', data); // Display the data in the browser console
-        console.log("Complete Data:", data);
-    // main.js
-
 var width = 800;
 var height = 600;
 
@@ -58,8 +50,6 @@ var projection = d3.geo.mercator()
 // Create a path generator for the UK GeoJSON
 var path = d3.geo.path().projection(projection);
 
-
-
 // Load the UK GeoJSON data
 d3.json("https://yamu.pro/gb.json", function (error, ukData) {
     // Append a path for the UK boundary
@@ -67,12 +57,8 @@ d3.json("https://yamu.pro/gb.json", function (error, ukData) {
     if (error) {
       console.error('Error loading GeoJSON:', error);
     } else {
-      // Your code for rendering the map goes here
-// Get the new number of plots based on the slider value
-    
-filteredData = data.slice(0, numPlots);
-      
-console.log('Filtered data:', filteredData);
+      //Slices the data so that a certain number of data points are drawn depending on the slider position
+sliderData = data.slice(0, numPlots);
     svg
         .selectAll("path")
         .data(ukData.features)
@@ -81,10 +67,12 @@ console.log('Filtered data:', filteredData);
         .attr("d", path)
         .style("fill", "lightgreen")
         .style("stroke", "gray");
+
+      
         //data point
         var point = svg.append("g");
       point.selectAll("circle")
-      .data(filteredData)
+      .data(sliderData)
       .enter().append("circle")
       .attr("class", "point")
       .attr("cx", function(d) {
@@ -93,40 +81,32 @@ console.log('Filtered data:', filteredData);
       .attr("cy", function(d) {
           return projection([d.lng, d.lat])[1];
       })
-      .attr("r", 0) // Start with a radius of 0
-  .transition() // Apply a transition
-  .attr("r", 3) // Final radius
-  .duration(500) // Set the duration of the transition in milliseconds
+        //Animate by changing the radius of the plots
+      .attr("r", 0)
+  .transition()
+  .attr("r", 3) 
+  .duration(500) 
   .each("end", function () {
-    // Transition end callback
     d3.select(this).on("mouseover", handleMouseOver).on("mouseout", handleMouseOut);
   });
         // Show details when hovering over a point
         
       onDataLoaded();
     }
-  
-  // You can style the points as desired
-        });
-        
-        console.log("Circles:", svg.selectAll(".point"));
-        
+        }); 
       }
-      sliderValue.textContent = this.value;
-
-    
-    
+      sliderValue.textContent = this.value;  
     // Remove previous plots
     svg.selectAll(".point").remove();
     });
-    var isLoading = false; // Flag to track whether data is currently being loaded
-
+    var isLoading = false; // Flag to track whether data is currently being loaded, to avoid data being called multiple times
+  
     document.getElementById('refreshButton').addEventListener('click', refreshButton);
     function refreshButton(){
       if (!isLoading) {
         var sliderValue = document.getElementById('plotSlider').value;
-        isLoading = true; // Set the flag to indicate that data is being loaded
-        // Remove previous data points before loading new data
+        isLoading = true;
+        // Removes any plots on the map before adding new ones
         svg.selectAll(".point").remove();
 
         // Call the LoadData function to load and render data
